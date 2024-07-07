6d29ce5036c970771ae2ecd175b9c3d57d26f4c4
Doit-Ay <125307797+Doit-Ay@users.noreply.github.com>
2024-07-07 18:52:25 +0530

    Add files via upload

---
+++ ATM Interface/BankAccount.java
@@ -0,0 +1,29 @@
+public class BankAccount {
+    private double balance;
+
+    public BankAccount(double initialBalance) {
+        this.balance = initialBalance;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+        } else {
+            System.out.println("Invalid deposit amount");
+        }
+    }
+
+    public boolean withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+            return true;
+        } else {
+            System.out.println("Invalid withdrawal amount or insufficient balance");
+            return false;
+        }
+    }
+}
